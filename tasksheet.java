f3b870086c25f5fd80ce2138bd115a59953a3f62
TOMASLOVE <164284042+TOMASLOVE@users.noreply.github.com>
2024-04-28 07:19:33 +0000

    Done adding output

---
+++ tasksheet.java
@@ -0,0 +1,10 @@
+public class tasksheet{
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        for(int i = 1; i<=check_number;i++){
+            message = (1%2==0) ? i+" is even number"
+            System.out.println(message);
+        }
+    }
+}
